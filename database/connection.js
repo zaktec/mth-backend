8ad17101da3f5f604525d0724a8d51fbaf562001
zaktec <csheraz@hotmail.com>
2023-09-14 19:53:56 +0100

    new new changes

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+/* const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -22,4 +22,30 @@ dbConnection.connect(() => {
     console.log('DB connection success !!');
 });
 
-module.exports = dbConnection;
+module.exports = dbConnection; */
+
+
+
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const ENV = process.env.NODE_ENV  ||  "development";
+const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
+
+//console.log (ENV)
+
+require('dotenv').config({
+ path: pathToCorrectEnvFile,
+});
+
+const dbConnection = new Pool();
+
+//console.log(">>>>>>",process.env.PGDATABASE)
+
+if (!process.env.PGDATABASE){
+    throw new Error ("No PGDATABSE configured");
+}
+
+
+module.exports = dbConnection 
