7c3dc8268694d332bdf8a7dece2fbf5bc32d8a94
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2024-03-27 17:32:38 +0200

    Changed login page to landing page

--- server/v1/routes/quizzeRoute.js
+++ server/v1/routes/quizzeRoute.js
@@ -20,9 +20,11 @@ router
     .patch('/update-quizzes/:quiz_id', userAuthorization(['admin']), updateQuizById)
     .delete('/delete-quizzes/:quiz_id', userAuthorization(['admin']), deleteQuizById)
     .get('/get-quizzes', userAuthorization(['admin', 'tutor', 'student']), getQuizzes)
+    .post('/post-reset-quiz/:studentquiz_id', userAuthorization(['tutor']), updateStudentQuizResult)
     .post('/post-student-quizzes/:student_id/:quiz_id', userAuthorization(['tutor']), postStudentQuiz)
     .get('/get-student-quizzes/:student_id', userAuthorization(['tutor', 'student']), getStudentQuizzes)
     .post('/post-tutor-feedback/:studentquiz_id', userAuthorization(['tutor']), updateStudentQuizResult)
+    .post('/post-student-feedback/:studentquiz_id', userAuthorization(['student']), updateStudentQuizResult)
     .post('/post-student-quiz-result/:studentquiz_id', userAuthorization(['student']), postStudentQuizResult);
 
 module.exports = router;
